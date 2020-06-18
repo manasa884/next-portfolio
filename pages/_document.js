8cc97346ae34e2c5125198581161a26aa2e33174
ManasaTipparam <manasa.tipparam@target.com>
2020-06-18 02:04:06 -0700

    server style sheets

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import React from 'react'
+import { ServerStyleSheets } from '@material-ui/styles'
 
 class CustomDocument extends Document {
   render() {
@@ -19,5 +20,30 @@ class CustomDocument extends Document {
     );
   }
 }
+
+CustomDocument.getInitialProps = async ctx => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />)
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      <React.Fragment key="styles">
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </React.Fragment>
+    ]
+  }
+}
+
 
 export default CustomDocument;
