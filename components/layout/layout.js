6cc4c2516c32fb95b22f49212161bc9bfc16b4c5
ManasaTipparam <manasa.tipparam@target.com>
2020-06-18 03:23:31 -0700

    layout style

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,22 +1,39 @@
 import { Grid, Typography } from '@material-ui/core'
 import Sidebar from '../navigation/sidebar'
-import styles from './layout.module.scss'
 
 const Layout = props => {
   return (
     <>
-    <Typography className={styles.brand}>MT</Typography>
-    <Grid container spacing={3} className={styles.grid}>
-      <Grid item xs={2}>
-        <Sidebar/>
+      <Typography className="brand">MT</Typography>
+      <Grid container spacing={3} className="grid">
+        <Grid item xs={2}>
+          <Sidebar/>
+        </Grid>
+        <Grid item xs={8} className="childrenContainer">
+          {props.children}
+        </Grid>
+        <Grid item xs={2}>
+        </Grid>
       </Grid>
-      <Grid item xs={8} className={styles.childrenContainer}>
-        {props.children}
-      </Grid>
-      <Grid item xs={2}>
-        
-      </Grid>
-    </Grid>
+      <style>{`
+        .brand {
+          font-family: "Muli", sans-serif;
+          font-weight: 700;
+          font-size: 1.8rem;
+          color: #333945;
+          margin: 40px 60px;
+          position: fixed;
+          letter-spacing: 2px;
+        }
+
+        .grid {
+          min-width: 900px;
+        }
+
+        .childrenContainer {
+          margin: 30px;
+        }
+      `}</style>
     </>
   )
 }
